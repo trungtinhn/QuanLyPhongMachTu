238532bf67b6b515a73b6a3ab2d29580882e3730
phongidoit <93896974+phongidoit@users.noreply.github.com>
2023-05-25 11:09:53 +0700

    Start on DanhSachKhamBenh

--- QuanLyPhongMachTu/UserControls/DanhSachKhamBenh.Designer.cs
+++ QuanLyPhongMachTu/UserControls/DanhSachKhamBenh.Designer.cs
@@ -29,25 +29,38 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.textBox1.Location = new System.Drawing.Point(196, 117);
+            this.textBox1.Location = new System.Drawing.Point(173, 131);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Hello world";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hello world";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // DanhSachKhamBenh
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "DanhSachKhamBenh";
-            this.Size = new System.Drawing.Size(416, 331);
+            this.Size = new System.Drawing.Size(818, 495);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- QuanLyPhongMachTu/UserControls/DanhSachKhamBenh.cs
+++ QuanLyPhongMachTu/UserControls/DanhSachKhamBenh.cs
@@ -21,5 +21,10 @@ namespace QuanLyPhongMachTu.UserControls
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
