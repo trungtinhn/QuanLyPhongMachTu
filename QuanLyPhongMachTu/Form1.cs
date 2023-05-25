d70c36fd37f94fe0c6325713d6aa99fa42a21f3d
phongidoit <93896974+phongidoit@users.noreply.github.com>
2023-05-25 10:01:34 +0700

    Test1

--- QuanLyPhongMachTu/Form1.cs
+++ QuanLyPhongMachTu/Form1.cs
@@ -16,5 +16,10 @@ namespace QuanLyPhongMachTu
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
